de6abca2ce371b4818e1593577839a37005df660
Liya Russom <leongza@riseup.net>
2013-08-06 12:30:18 -0700

    add secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 CPTB::Application.config.secret_token = '25f80ed73d1ec707150e179e25dded43dde99ba6f1e4c2be617c29c356a0ef783311b761feead1036de9f9820f758e266cbff3e014975e719659b5bcec154161'
+CPTB::Application.config.secret_key_base = '364e7f2c450f2426549b80a05d6f502fa9378b5d2255cb81b9f05749041895322abdd067f383f9dab1cb952616b367d9b5737ddf894ca2e9bc365a5973520497'
